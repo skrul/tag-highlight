bafd60cdd32e06cc9f116ee65058e982012fdf69
Steve Krulewitz <skrulx@gmail.com>
2015-05-01 21:44:39 -0700

    Web front end.

---
+++ static/tag_highlight.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var statsTable = (function() {
+  var tbody = $("#stats-table > tbody")
+  var selectedRow = -1;
+  var rows = [];
+  var setData = function(rowData) {
+    rows = rowData;
+    tbody.empty();
+    for (var i = 0; i < rows.length; i++) {
+      var row = rows[i];
+      var tr = $("<tr>");
+      tr.append("<td>" + row["name"] + "</td>");
+      tr.append("<td>" + row["count"] + "</td>");
+      tbody.append(tr);
+    }
+  }
+
+  var selectedRowData = function() {
+    if (selectedRow < 0) {
+      return null;
+    } else {
+      return rows[selectedRow];
+    }
+  }
+  
+  tbody.click(function(e) {
+    if (selectedRow >= 0) {
+      tbody.children().eq(selectedRow).removeClass('info');
+    }
+    var tr = $(e.target).parent();
+    if (tr.index() == selectedRow) {
+      selectedRow = -1;
+    } else {
+      tr.addClass('info');
+      selectedRow = tr.index();
+    }
+    $("#stats-table").trigger("row-selected");
+  });
+
+  return {
+    selectedRowData: selectedRowData,
+    setData: setData
+  }
+})();
+
+
+var sourceView = (function() {
+  var root = $("#source-view");
+  var selectedTag = null;
+
+  var setSource = function(html) {
+    root.html(html);
+    highlightTag(null);
+  }
+
+  var highlightTag = function(tagName) {
+    if (selectedTag) {
+      root.find(".tag-" + selectedTag).css("background-color", "transparent");
+    }
+    if (tagName) {
+      root.find(".tag-" + tagName).css("background-color", "yellow");
+    }
+    selectedTag = tagName;
+  }
+  
+  return {
+    setSource: setSource,
+    highlightTag: highlightTag
+  }
+})();
+
+$(function() {
+  $("#form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: "/api/v1/describe-page",
+      data: {url: $("#url").val()},
+      type: "GET",
+      dataType: "json",
+      success: function(json) {
+	//if error...
+        sourceView.setSource(json.highlighted_html);
+        
+        var rows = []
+        for (var name in json.tag_counts) {
+          rows.push({name: name, count: json.tag_counts[name]});
+        }
+        rows.sort(function(a, b) { return b.count - a.count });
+        statsTable.setData(rows);
+
+        $("#stats-table").on("row-selected", function() {
+          var data = statsTable.selectedRowData();
+          if (data) {
+            sourceView.highlightTag(data["name"]);
+          } else {
+            sourceView.highlightTag(null);
+          }
+        });
+
+      }
+    });
+  });
+});
