bbad259d5228a1bfc1bd674f436d6d53f26ec4c1
Steve Krulewitz <skrulx@gmail.com>
2015-05-02 20:20:09 -0700

    Another EBS fix.

--- application.py
+++ application.py
@@ -4,9 +4,9 @@ import flask_bootstrap
 import tag_highlight
 
 application = flask.Flask(__name__)
+flask_bootstrap.Bootstrap(application)
 application.register_blueprint(tag_highlight.app)
 application.debug = True
 
 if __name__ == '__main__':
-    flask_bootstrap.Bootstrap(application)
     application.run()
